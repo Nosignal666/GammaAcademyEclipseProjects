7e50c324c865eac9462478e3615c647791fea276
Nosignal666 <cosimo.moretti@outlook.it>
2020-03-19 19:08:12 +0100

    1)Aggiunto come JRE d'ambiente il JDK1.8 2)Aggiornato anagraficaService, DB Heroku hardcoded

--- test-springboot/src/main/java/com/gamma/wave/test_springboot/MessageController.java
+++ test-springboot/src/main/java/com/gamma/wave/test_springboot/MessageController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,16 @@ public class MessageController {
 		};
 		return new ResponseEntity<List<Message>>(messages,HttpStatus.OK);
 	}
-
+	@RequestMapping(value="api/messages/{id}",
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Message> getMessage(@PathVariable("id") Integer id){
+		if(id!=1){
+			return new ResponseEntity<Message>(HttpStatus.NOT_FOUND);
+		}
+		else{
+			return new ResponseEntity<Message>(new Message(1,"I'm Java Boss"),HttpStatus.OK);
+		}
+	}
+	
 }
